ae90f83f224fe5539df63861210874926ae5ba40
Pablo <ezekielgonzalezr@gmail.com>
2023-04-24 11:05:30 -0600

    Add array object and create form variables

---
+++ index.js
@@ -0,0 +1,28 @@
+const book = [
+    {
+        title: "Song of Ice & fire",
+        author: "George RR"
+    },
+    {
+        title: "Song of Ice & fire",
+        author: "George RR"
+    },
+    {
+        title: "Song of Ice & fire",
+        author: "George RR"
+    },
+    {
+        title: "Song of Ice & fire",
+        author: "George RR"
+    }
+];
+
+const form = document.querySelector("#form");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const formButton = document.querySelector("submit-button");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+})
