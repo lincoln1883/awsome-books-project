554ee487386a18115ff5ea00f2bb4c3faae9745a
lincoln gibson <lincolngibson7@gmail.com>
2023-04-24 14:55:23 -0300

    Add functionality for storing form data in local storage.

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-const book = [
-    {
-        title: "Song of Ice & fire",
-        author: "George RR"
-    },
-    {
-        title: "Song of Ice & fire",
-        author: "George RR"
-    },
-    {
-        title: "Song of Ice & fire",
-        author: "George RR"
-    },
-    {
-        title: "Song of Ice & fire",
-        author: "George RR"
-    }
-];
+// const book = [
+//   {
+//     title: 'Song of Ice & fire',
+//     author: 'George RR',
+//   },
+//   {
+//     title: 'Song of Ice & fire',
+//     author: 'George RR',
+//   },
+//   {
+//     title: 'Song of Ice & fire',
+//     author: 'George RR',
+//   },
+//   {
+//     title: 'Song of Ice & fire',
+//     author: 'George RR',
+//   },
+// ];
 
-const form = document.querySelector("#form");
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const formButton = document.querySelector("submit-button");
+// form declaration
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+const form = document.querySelector('#form');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const formButton = document.querySelector('#submit-button');
 
-})
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	form.submit();
+});
+
+// const BookData = { title: title.value, author: author.value };
+
+formButton.addEventListener('click', (e) => {
+	e.preventDefault();
+	const newBook = {
+		title: title.value,
+		author: author.value,
+	};
+
+	localStorage.setItem('book', JSON.stringify(newBook));
+});
